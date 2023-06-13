fcd0f9f36f03bf16a8a9cc9e5fdd96daeb83d703
Michael Gisler <michael.gisler@noser.com>
2023-06-13 19:00:44 +0200

    more tests added

--- app/tests/calculator.test.js
+++ app/tests/calculator.test.js
@@ -11,3 +11,11 @@ test('adds -5 + 20 to equal 15', () => {
 test('adds 3 ^ 4 to equal 81', () => {
   expect(calculator.pot(3, 4)).toBe(81);
 });
+
+test('divides 10 by 0 to equal undefined', () => {
+  expect(calculator.div(10, 0)).toBe(undefined);
+});
+
+test('divides 10 by 2 to equal undefined', () => {
+  expect(calculator.div(10, 2)).toBe(5);
+});
